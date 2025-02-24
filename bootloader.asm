56e4ed531793bd33cf7af1098ea8f8eb03493fbe
moneybagsmahoney <79066541+WheeledCord@users.noreply.github.com>
2025-02-24 14:18:20 +1300

    fixed a lot of stuff (see discription)
    fix bootloader segment registers & stack % wrong disk load call & jump to 0x1000:0000 not just 0x1000

--- bootloader.asm
+++ bootloader.asm
@@ -3,22 +3,25 @@ ORG 0x7C00
 
 start:
     cli
-    mov ax, 0x07C0
+    mov ax, 0x0000  ; Set up segment registers correctly
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
-    mov sp, 0x7C00
+
+    mov sp, 0x7E00  ; Set stack higher in memory
 
     mov si, message
     call print_string
 
-    ; load kernal by reading from disk
-    mov bx, 0x1000  ; load address for kernel
+    ; Load kernel by reading from disk
+    mov ax, 0x1000  ; Segment where kernel will be loaded
+    mov es, ax
+    mov bx, 0x0000  ; Offset in segment
     mov dh, 2       ; # of sectors to read
     call disk_load
 
-    jmp 0x1000      ; jump to loaded kernel
+    jmp 0x1000:0000  ; Jump to loaded kernel
 
 print_string:
     lodsb
@@ -31,14 +34,14 @@ done:
     ret
 
 disk_load:
-    mov ah, 0x02    ; bios disk read function
+    mov ah, 0x02    ; BIOS disk read function
     mov al, dh      ; # of sectors to read
-    mov ch, 0x00    ; cylinder 0
-    mov dh, 0x00    ; head 0
-    mov cl, 0x02    ; start reading at sector 2
-    mov dl, 0x80    ; first hard disk
+    mov ch, 0x00    ; Cylinder 0
+    mov dh, 0x00    ; Head 0
+    mov cl, 0x02    ; Start reading at sector 2
+    mov dl, 0x80    ; First hard disk
     int 0x13
-    jc error        ; if carry flag set, print error
+    jc error        ; If carry flag set, print error
     ret
 
 error:
@@ -49,5 +52,5 @@ error:
 message db 'Booting tetrOS...', 0
 disk_fail db 'stupid ass disk broke', 0
 
-times 510-($-$$) db 0
-dw 0xAA55
+times 510-($-$$) db 0  ; Pad to 512 bytes
+dw 0xAA55               ; Boot signature
