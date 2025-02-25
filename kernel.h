ae38a94d19dc00be8fcc6ccd4b7404d6f5931304
moneybagsmahoney <79066541+WheeledCord@users.noreply.github.com>
2025-02-26 10:58:14 +1300

    Update kernel.h

--- kernel.h
+++ kernel.h
@@ -1,31 +1,31 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
-// VGA text mode color attribute
+//vga text mode color attribute
 #define WHITE_TXT 0x07
 
-// --- VGA Functions ---
+//vga functions
 void k_clear_screen();
 unsigned int k_printf(char *message, unsigned int line);
 void disable_cursor();
 
-// --- Utility Functions ---
+//utility functions
 void itoa(int value, char *str, int base);
 
-// --- I/O Port Functions ---
+//io port functions
 void outb(unsigned short port, unsigned char data);
 unsigned char inb(unsigned short port);
 
-// --- PIC & Interrupt Setup ---
+//pic and interrupt setup
 void pic_remap();
 void k_install_idt();
 void idt_load(unsigned int);
 
-// --- Device Driver Initialization ---
+//device driver initialization
 void init_timer();
 void init_keyboard();
 
-// --- Kernel Main Function ---
+//kernel main function
 void k_main();
 
-#endif // KERNEL_H
+#endif //KERNEL_H
