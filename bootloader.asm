a76d69e6d6305b4cc5f4bf674657765f18b64162
moneybagsmahoney <79066541+WheeledCord@users.noreply.github.com>
2025-02-25 22:55:42 +1300

    make it big again

--- bootloader.asm
+++ bootloader.asm
@@ -1,17 +1,41 @@
-bits 32		;nasm directive
+;multiboot compliant bootloader (i think) & interrupt stubs
 section .text
-	;multiboot spec
-	align 4
-	dd 0x1BADB002			;magic
-	dd 0x00				;flags
-	dd - (0x1BADB002 + 0x00)	;checksum
+align 4
+dd 0x1badb002           ;multiboot magic number
+dd 0x00                 ;flags
+dd -(0x1badb002+0x00)    ;checksum
 
 global start
-extern k_main	;k_main is defined in the kernel.c
+extern k_main
+
+;declare external c interrupt handlers.
+extern timer_handler
+extern keyboard_handler
 
 start:
-	cli  ; stop interrupts
+    cli             ;disable interrupts
+    call k_main     ;call the kernel main function
+    hlt             ;halt if k_main ever returns
 
-	call k_main
+;idt loading routine
+global idt_load
+idt_load:
+    mov eax,[esp+4] ;get pointer to our idt_ptr structure
+    lidt [eax]      ;load the idt register
+    ret
 
-	hlt ; halt the CPU
+;timer interrupt stub
+global timer_handler_stub
+timer_handler_stub:
+    pusha         ;save registers.
+    call timer_handler ;call c timer handler
+    popa          ;restore registers
+    iret          ;return from interrupt
+
+;keyboard interrupt stub
+global keyboard_handler_stub
+keyboard_handler_stub:
+    pusha         ;save registers
+    call keyboard_handler ;call c keyboard handler
+    popa          ;restore registers
+    iret          ;return from interrupt
