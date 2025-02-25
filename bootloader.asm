e9ac20320742678892a9b33686523c4e211224d4
moneybagsmahoney <79066541+WheeledCord@users.noreply.github.com>
2025-02-26 11:00:09 +1300

    bootloader

--- bootloader.asm
+++ bootloader.asm
@@ -1,41 +1,41 @@
-; bootloader.asm - Multiboot compliant bootloader & interrupt stubs
+;bootloader.asm multiboot compliant bootloader and interrupt stubs
 section .text
     align 4
-    dd 0x1BADB002           ; Multiboot magic number
-    dd 0x00                 ; Flags
-    dd -(0x1BADB002 + 0x00)  ; Checksum
+    dd 0x1BADB002           ;multiboot magic number
+    dd 0x00                 ;flags
+    dd -(0x1BADB002 + 0x00)  ;checksum
 
 global start
 extern k_main
 
-; Declare external C interrupt handlers.
+;declare external c interrupt handlers
 extern timer_handler
 extern keyboard_handler
 
 start:
-    cli             ; Disable interrupts.
-    call k_main     ; Call the kernel main function.
-    hlt             ; Halt if k_main ever returns.
+    cli             ;disable interrupts
+    call k_main     ;call the kernel main function
+    hlt             ;halt if k_main ever returns
 
-; --- IDT Loading Routine ---
+;idt loading routine
 global idt_load
 idt_load:
-    mov eax, [esp+4]  ; Get pointer to our idt_ptr structure.
-    lidt [eax]        ; Load the IDT register.
+    mov eax, [esp+4]  ;get pointer to idt_ptr structure
+    lidt [eax]        ;load idt register
     ret
 
-; --- Timer Interrupt Stub ---
+;timer interrupt stub
 global timer_handler_stub
 timer_handler_stub:
-    pusha                  ; Save registers.
-    call timer_handler     ; Call C timer handler.
-    popa                   ; Restore registers.
-    iret                   ; Return from interrupt.
+    pusha                  ;save registers
+    call timer_handler     ;call c timer handler
+    popa                   ;restore registers
+    iret                   ;return from interrupt
 
-; --- Keyboard Interrupt Stub ---
+;keyboard interrupt stub
 global keyboard_handler_stub
 keyboard_handler_stub:
-    pusha                   ; Save registers.
-    call keyboard_handler   ; Call C keyboard handler.
-    popa                    ; Restore registers.
-    iret                    ; Return from interrupt.
+    pusha                   ;save registers
+    call keyboard_handler   ;call c keyboard handler
+    popa                    ;restore registers
+    iret                    ;return from interrupt
