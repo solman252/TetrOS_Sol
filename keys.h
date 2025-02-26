e6014dd7b7c096c2ec529eae74d0444c8a42cdf0
moneybagsmahoney <79066541+WheeledCord@users.noreply.github.com>
2025-02-27 12:33:17 +1300

    Create keys.h

---
+++ keys.h
@@ -0,0 +1,15 @@
+#ifndef KEYS_H
+#define KEYS_H
+
+#define KEY_BACKSPACE      0x0E
+#define KEY_ENTER          0x1C
+#define KEY_SPACE          0x39
+#define KEY_SHIFT_PRESS    0x2A
+#define KEY_SHIFT_RELEASE  0xAA
+
+#define KEY_UP             0x48
+#define KEY_DOWN           0x50
+#define KEY_LEFT           0x4B
+#define KEY_RIGHT          0x4D
+
+#endif
