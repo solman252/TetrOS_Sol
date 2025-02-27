94509715ac7947feb27527992675be2bbfa5c25e
Solman252 <undertalelover909@gmail.com>
2025-02-28 11:21:06 +1300

    colors fixed!

--- kernel.c
+++ kernel.c
@@ -21,21 +21,30 @@ void set_vga_palette(int color_index, int r, int g, int b) {
 
 void set_custom_palette() {
     set_vga_palette(0,  0,  0,  0);   // BLACK
-    set_vga_palette(1,  63, 63, 63);   // WHITE
-    set_vga_palette(2,  32, 32, 32);   // GRAY
-    set_vga_palette(3,  0,  63, 63);   // CYAN
+    set_vga_palette(1,  63, 63, 63);  // WHITE
+    set_vga_palette(2,  32, 32, 32);  // GRAY
+    set_vga_palette(3,  0,  55, 55);  // CYAN
     set_vga_palette(4,  0,  0,  63);  // BLUE
-    set_vga_palette(5,  63, 32, 0);  // ORANGE
-    set_vga_palette(6,  63, 0, 0);  // YELLOW
-    set_vga_palette(7,  0,  63, 0);  // GREEN
-    set_vga_palette(8,  63, 0,  63);  // PURPLE
-    set_vga_palette(9,  63, 0,  0);  // RED
-    set_vga_palette(10, 16, 63, 63);  // LIGHT_CYAN
-    set_vga_palette(11, 63, 48, 16);  // LIGHT_ORANGE
-    set_vga_palette(12, 63, 63, 16);  // LIGHT_YELLOW
-    set_vga_palette(13, 16, 63, 16);  // LIGHT_GREEN
-    set_vga_palette(14, 63, 16, 63);  // LIGHT_PURPLE
-    set_vga_palette(15, 63, 16, 16);  // LIGHT_RED
+    set_vga_palette(5,  63, 32, 0);   // ORANGE
+    set_vga_palette(7,  0,  63, 0);   // GREEN
+
+    set_vga_palette(20,  55, 55, 0);  // YELLOW               6 ISNT SET, USE 0x14 == 20
+    set_vga_palette(56,  63, 0,  63); // PURPLE               8 ISNT SET, USE 0x38 == 56
+    set_vga_palette(57,  63, 0,  0);  // RED                  9 ISNT SET, USE 0x39 == 57
+    set_vga_palette(58, 50, 63, 63);  // LIGHT_CYAN           10 ISNT SET, USE 0x3A == 58
+    set_vga_palette(59, 63, 48, 32);  // LIGHT_ORANGE         11 ISNT SET, USE 0x3B == 59
+    set_vga_palette(60, 63, 63, 50);  // LIGHT_YELLOW         12 ISNT SET, USE 0x3C == 60
+    set_vga_palette(61, 50, 63, 50);  // LIGHT_GREEN          13 ISNT SET, USE 0x3D == 61
+    set_vga_palette(62, 63, 48, 63);  // LIGHT_PURPLE         14 ISNT SET, USE 0x3E == 62
+    set_vga_palette(63, 63, 32, 32);  // LIGHT_RED            15 ISNT SET, USE 0x3F == 63
+    // 0x06 ISNT SET
+    // 0x08 ISNT SET
+    // 0x09 ISNT SET
+    // 0x0A ISNT SET
+    // 0x0B ISNT SET
+    // 0x0D ISNT SET
+    // 0x0E ISNT SET
+    // 0x0F ISNT SET
 }
 #define BLACK 0x00
 #define WHITE 0x01
@@ -410,19 +419,20 @@ void draw_grid() {
     }
 
     // choose the color based on current_shape using existing macros
-    char col = WHITE;
+    char col = GRAY;
+    char col_inner = WHITE;
     switch(current_shape) {
-        case 0: col = YELLOW; break; // shape_o
-        case 1: col = CYAN; break; // shape_i
-        case 2: col = GREEN; break; // shape_s
-        case 3: col = RED; break; // shape_z
-        case 4: col = ORANGE; break; // shape_l
-        case 5: col = BLUE; break; // shape_j
-        case 6: col = PURPLE; break; // shape_t
+        case 0: col = YELLOW; col_inner = LIGHT_YELLOW; break; // shape_o
+        case 1: col = CYAN; col_inner = LIGHT_CYAN; break; // shape_i
+        case 2: col = GREEN; col_inner = LIGHT_GREEN; break; // shape_s
+        case 3: col = RED; col_inner = LIGHT_RED; break; // shape_z
+        case 4: col = ORANGE; col_inner = LIGHT_ORANGE; break; // shape_l
+        case 5: col = BLUE; col_inner = LIGHT_BLUE; break; // shape_j
+        case 6: col = PURPLE; col_inner = LIGHT_PURPLE; break; // shape_t
     }
     unsigned char attr;
     if (highlight_bg) {
-        attr = (col << 4) | BLACK;
+        attr = (col << 4) | col_inner;
     } else {
         attr = col;
     }
@@ -519,6 +529,7 @@ void kernel_loop() {
 }
 
 void k_main() {
+    outb(0x3C6, 0xFF);
     set_custom_palette();
     k_clear_screen();
     disable_cursor();
